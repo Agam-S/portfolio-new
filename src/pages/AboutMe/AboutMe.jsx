e7820dd48c43efc8cd1151683251aa7936f79d93
Agam-S <singh.agam0009@gmail.com>
2024-03-12 17:42:55 +1100

    about me

--- src/pages/AboutMe/AboutMe.jsx
+++ src/pages/AboutMe/AboutMe.jsx
@@ -1,11 +1,21 @@
 import "./AboutMe.css";
+import { motion } from "framer-motion";
 
 const AboutMe = () => {
   return (
     <>
-      <h1 id="AboutMe" className="aboutme container">
-        About Me
-      </h1>
+      <div className="containerAboutMe">
+        <h1 id="AboutMe" className="aboutme">
+          About Me
+        </h1>
+        <motion.p className="textAboutMe">
+          Hello, thank you for visiting my portfolio. I have created several
+          full-stack applications both for personal projects and as part of a
+          team. I have 4 years of experience working with JavaScript
+          frameworks/libraries such as Express and React as well as languages
+          like C#, Php, and Java.
+        </motion.p>
+      </div>
     </>
   );
 };
